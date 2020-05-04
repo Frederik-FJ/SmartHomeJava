e0744cb5c1cc8f3b2c4a3ed64f6619640a4c53a2
Frederik-FJ <frederik04.fj@gmail.com>
2020-05-04 15:45:27 +0200

    Added mail-sender

--- code/src/main/java/information/Information.java
+++ code/src/main/java/information/Information.java
@@ -6,5 +6,11 @@ public class Information {
 	
 	public static String FritzBoxIP = "192.168.178.1"; // Change to the IP of your FritzBox, default is 192.168.178.1
 	public static String FritzBoxPassword = "password";
+	
+	public static String senderMail = "senderMail";
+	public static String receiver = "recieverAddress1,receiverAddress2";
+	public static String mailUser = "mailUser";
+	public static String mailPw = "mailPw";
+	public static String mailName = "mailName";
 
 }
---
+++ code/src/main/java/mail/MailSender.java
@@ -0,0 +1,76 @@
+package mail;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import logger.Logger;
+
+public class MailSender {
+	
+	protected Session mailSession;
+	
+	public void login(String smtpHost, int smtpPort, String user, String pw) {
+		Properties props = new Properties();
+		 props.put("mail.smtp.host", smtpHost);
+        props.put("mail.smtp.socketFactory.port", smtpPort);
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", smtpPort);
+        
+        Authenticator auth = new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(user, pw);
+            }
+        };
+
+        this.mailSession = Session.getDefaultInstance(props, auth);
+        Logger.log("Mail", "Info", "Logged in into the mail sever");
+	}
+	
+	public void sendMail(String senderMail, String senderName, String receiverAddress, String subject, String message) throws MessagingException, IllegalStateException, UnsupportedEncodingException {
+        if(mailSession == null){
+            throw new IllegalStateException("Du musst dich zuerst einloggen. Rufe dazu die login Methode auf");
+        }
+
+        MimeMessage msg = new MimeMessage(mailSession);
+        msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+        msg.addHeader("format", "flowed");
+        msg.addHeader("Content-Transfer-Encoding", "8bit");
+
+        msg.setFrom(new InternetAddress(senderMail, senderName));
+        msg.setReplyTo(InternetAddress.parse(senderMail, false));
+        msg.setSubject(subject, "UTF-8");
+        msg.setText(message, "UTF-8");
+        msg.setSentDate(new Date());
+
+        msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(receiverAddress, false));
+
+        Logger.log("Mail", "Info", "send mail...");
+        send(msg);
+        Logger.log("Mail", "Info", "Mail sended");
+    }
+	
+	public void sendMail(String senderMail, String receiverAddress, String message) throws IllegalStateException, UnsupportedEncodingException, MessagingException {
+		this.sendMail(senderMail, "Home", receiverAddress, "Home", message);
+	}
+	
+	public void send(String senderMail, String receiverAddress, String subject, String message) throws IllegalStateException, UnsupportedEncodingException, MessagingException {
+		this.sendMail(senderMail, "Home", receiverAddress, subject, message);
+	}
+	
+	
+	private void send(Message msg) throws MessagingException {
+		Transport.send(msg);
+	}
+
+}
