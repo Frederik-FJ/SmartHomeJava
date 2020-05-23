c4f9c22703951e90da1e19542b3ec64649c61967
Frederik-FJ <frederik04.fj@gmail.com>
2020-05-23 22:02:46 +0200

    Updated Information.java

--- src/main/java/information/Information.java
+++ src/main/java/information/Information.java
@@ -2,16 +2,20 @@ package information;
 
 public class Information {
 
-    public static String SonosIP = "Sonos IP";
+    public static String SONOS_IP = "SONOS IP";
 
-    public static String FritzBoxIP = "192.168.178.1"; // Change to the IP of your FritzBox, default is 192.168.178.1
-    public static String FritzBoxPassword = "Password";
+    public static String FRITZBOX_IP = "192.168.178.1"; // Change to the IP of your FritzBox, default is 192.168.178.1
+    public static String FRITZBOX_PW = "Password";
 
-    public static String pythonCmd = "python";
+    public static String HOMEMATIC_IP = "HOMEMATIC IP";
+    public static String HOMEMATIC_NAME = "HOMEMATIC NAME";
+    public static String HOMEMATIC_PASSWORD = "HOMEMATIC Password";
 
-    public static String senderMail = "senderMail";
-    public static String receiver = "recieverAddress1,receiverAddress2";
-    public static String mailUser = "mailUser";
-    public static String mailPw = "mailPw";
-    public static String mailName = "mailName";
+    public static String PYTHON_CMD = "python";
+
+    public static String SENDER_MAIL = "senderMail";
+    public static String MAIL_RECEIVER = "recieverAddress1,receiverAddress2";
+    public static String MAIL_USER = "mailUser";
+    public static String MAIL_PW = "mailPw";
+    public static String MAIL_NAME = "mailName";
 }
